6c86b5fa0c68dcc0a4baf9f3b4c5526d8349ee76
zhuyk6 <zhuyk6@foxmail.com>
2023-04-04 17:41:45 +0800

    Add some examples about Svetlichny.

---
+++ qnn.py
@@ -0,0 +1,176 @@
+import numpy as np
+from mindquantum.simulator import Simulator
+from mindquantum.core.circuit import Circuit
+from mindquantum.core.parameterresolver import ParameterResolver
+from mindquantum.core.operators import Hamiltonian, QubitOperator
+from mindquantum.core.gates import RZ, RY
+from mindquantum.framework import MQAnsatzOnlyOps
+
+import mindspore as ms
+ms.context.set_context(mode=ms.context.PYNATIVE_MODE, device_target="CPU")
+
+def generate_circuit2(p1: int, p2: int) -> Circuit:
+    circ = Circuit()
+
+    circ += RZ(pr=ParameterResolver(
+        data={f"phi{p1}": -1},
+        const=np.pi,
+    )).on(0)
+    circ += RY(f"theta{p1}").on(0)
+
+    circ += RZ(pr=ParameterResolver(
+        data={f"phi{p2}": -1},
+        const=np.pi,
+    )).on(1)
+    circ += RY(f"theta{p2}").on(1)
+
+    return circ
+
+def generate_circuit3(p1: int, p2: int, p3: int) -> Circuit:
+    circ = Circuit()
+
+    circ += RZ(pr=ParameterResolver(
+        data={f"phi{p1}": -1},
+        const=np.pi,
+    )).on(0)
+    circ += RY(f"theta{p1}").on(0)
+
+    circ += RZ(pr=ParameterResolver(
+        data={f"phi{p2}": -1},
+        const=np.pi,
+    )).on(1)
+    circ += RY(f"theta{p2}").on(1)
+
+    circ += RZ(pr=ParameterResolver(
+        data={f"phi{p3}": -1},
+        const=np.pi,
+    )).on(2)
+    circ += RY(f"theta{p3}").on(2)
+
+    return circ
+
+
+class QNet2(ms.nn.Cell):
+    def __init__(self, grad_ops1, grad_ops2, grad_ops3, grad_ops4) -> None:
+        super().__init__()
+        self.w = ms.Parameter(ms.Tensor(np.random.uniform(-np.pi, np.pi, 8).astype(np.float32)), name="weight")
+        self.q_ops1 = MQAnsatzOnlyOps(grad_ops1)
+        self.q_ops2 = MQAnsatzOnlyOps(grad_ops2)
+        self.q_ops3 = MQAnsatzOnlyOps(grad_ops3)
+        self.q_ops4 = MQAnsatzOnlyOps(grad_ops4)
+    
+    def construct(self):
+        # w[[0, 1]] -- theta1, phi1
+        # w[[2, 3]] -- theta2, phi2
+        # w[[4, 5]] -- theta3, phi3
+        # w[[6, 7]] -- theta4, phi4
+        e1 = self.q_ops1(self.w[[0, 1, 4, 5]])
+        e2 = self.q_ops2(self.w[[0, 1, 6, 7]])
+        e3 = self.q_ops3(self.w[[2, 3, 4, 5]])
+        e4 = self.q_ops4(self.w[[2, 3, 6, 7]])
+        return -(e1 + e2 + e3 - e4)
+
+def num_to_params3(n: int):
+    b1 = 1 if (n & 4) > 0 else 0
+    b2 = 3 if (n & 2) > 0 else 2
+    b3 = 5 if (n & 1) > 0 else 4
+    return b1, b2, b3
+
+class QNet3(ms.nn.Cell):
+    def __init__(self, grads) -> None:
+        if len(grads) != 8:
+            raise ValueError("There should be 8 grad_ops.")
+        
+        super().__init__()
+        self.w = ms.Parameter(ms.Tensor(np.random.uniform(-np.pi, np.pi, 12).astype(np.float32)), name="weight")
+        
+        self.op0 = MQAnsatzOnlyOps( grads[0] )
+        self.op1 = MQAnsatzOnlyOps( grads[1] )
+        self.op2 = MQAnsatzOnlyOps( grads[2] )
+        self.op3 = MQAnsatzOnlyOps( grads[3] )
+        self.op4 = MQAnsatzOnlyOps( grads[4] )
+        self.op5 = MQAnsatzOnlyOps( grads[5] )
+        self.op6 = MQAnsatzOnlyOps( grads[6] )
+        self.op7 = MQAnsatzOnlyOps( grads[7] )
+        self.ops = [self.op0, self.op1, self.op2, self.op3, self.op4, self.op5, self.op6, self.op7]
+    
+    def construct(self):
+        def g(n: int):
+            b1, b2, b3 = num_to_params3(n)
+            return [b1*2, b1*2+1, b2*2, b2*2+1, b3*2, b3*2+1]
+
+        def f(n: int):
+            return self.ops[n](self.w[g(n)])
+        
+        return -( f(0) + f(1) + f(2) - f(3) - f(4) + f(5) + f(6) + f(7) )
+
+def check_dim(P: np.matrix, n: int, m: int) -> bool:
+    return P.shape == (n, m)
+
+def CHSH_qnn(rho: np.matrix):
+    if not check_dim(rho, 4, 4):
+        raise ValueError("CHSH only considers 2 qubits.")
+
+    sim = Simulator(backend="mqmatrix", n_qubits=2)
+    rho = np.asarray(rho)
+    sim.set_qs(rho)
+
+    circ1 = generate_circuit2(1, 3)
+    circ2 = generate_circuit2(1, 4)
+    circ3 = generate_circuit2(2, 3)
+    circ4 = generate_circuit2(2, 4)
+
+    ham = Hamiltonian(QubitOperator("Z0 Z1", 1))
+
+    grad_ops1 = sim.get_expectation_with_grad(ham, circ1)
+    grad_ops2 = sim.get_expectation_with_grad(ham, circ2)
+    grad_ops3 = sim.get_expectation_with_grad(ham, circ3)
+    grad_ops4 = sim.get_expectation_with_grad(ham, circ4)
+
+    qnet = QNet2(grad_ops1, grad_ops2, grad_ops3, grad_ops4)
+    optimizer = ms.nn.Adam(qnet.trainable_params(), learning_rate=0.1)
+    train_net = ms.nn.TrainOneStepCell(qnet, optimizer)
+
+    last = np.float32(0)
+    for i in range(1000):
+        f = train_net().asnumpy()
+        if i % 10 == 0:
+            # print(f"epoch = {i: 5}\testimite={-f[0]:.7f}")
+            if np.abs(f[0] - last) < 1e-7:
+                break
+            last = f[0]
+    return -last
+
+def Svetlichny_qnn(rho: np.matrix):
+    if not check_dim(rho, 8, 8):
+        raise ValueError("Svetlichny considers only 3 qubits.")
+    
+    sim = Simulator(backend="mqmatrix", n_qubits=3)
+    rho = np.asarray(rho)
+    sim.set_qs(rho)
+
+    circs = []
+    for i in range(8):
+        b1, b2, b3 = num_to_params3(i)
+        circs.append(generate_circuit3(b1, b2, b3))
+
+    ham = Hamiltonian(QubitOperator("Z0 Z1 Z2", 1))
+
+    grad_ops = []
+    for i in range(8):
+        grad_ops.append(sim.get_expectation_with_grad(ham, circs[i]))
+
+    qnet = QNet3(grads=grad_ops)
+
+    optimizer = ms.nn.Adam(qnet.trainable_params(), learning_rate=0.1)
+    train_net = ms.nn.TrainOneStepCell(qnet, optimizer)
+
+    last = np.float32(0)
+    for i in range(1000):
+        f = train_net().asnumpy()
+        if i % 10 == 0:
+            # print(f"epoch = {i: 5}\testimite={-f[0]:.7f}")
+            if np.abs(f[0] - last) < 1e-7:
+                break
+            last = f[0]
+    return -last
---
+++ run.py
@@ -0,0 +1,371 @@
+from qnn import *
+from typing import Tuple
+from typing import Optional
+import json
+import pandas
+
+def CHSH_max(rho: np.matrix) -> float:
+    """Calculate optimal S(rho).
+
+    Args:
+        rho (np.matrix): Density matrix of 2-qubit
+
+    Returns:
+        float: Optimal S(rho)
+    """
+    X = np.matrix([[0, 1], [1, 0]], dtype=np.complex128)
+    Y = np.matrix([[0, -1j], [1j, 0]], dtype=np.complex128)
+    Z = np.matrix([[1, 0], [0, -1]], dtype=np.complex128)
+    sigma = [X, Y, Z]
+    T = [[(rho @ np.kron(sigma[i], sigma[j])).trace()[0, 0] for j in range(3)] for i in range(3)]
+    T = np.matrix(T).real
+    U = T.T @ T
+    eigens = np.linalg.eigvals(U)
+    eigens.sort()
+    return 2.0 * np.sqrt(eigens[-1] + eigens[-2])
+
+def rand_state(n_qubits: int) -> np.ndarray:
+    d = 2**n_qubits
+    re = np.random.random((d,))
+    im = np.random.random((d,))
+    state = re + 1.0j * im
+    return state / np.linalg.norm(state)
+
+def rand_density_matrix(n_qubits: int, m: int) -> np.matrix:
+    d = 2**n_qubits
+    probabilities = np.random.random((m, ))
+    probabilities /= probabilities.sum()
+
+    rho = np.zeros((d, d), dtype=np.complex128)
+    rho = np.asmatrix(rho)
+    for p in probabilities:
+        state = np.asmatrix(rand_state(n_qubits))
+        rho += p * state.H @ state
+    return rho
+
+
+def run_random2():
+    m = 3
+    for epoch in range(100):
+        print(f"epoch: {epoch}")
+        rho_rand = rand_density_matrix(2, 3)
+        v_opt = CHSH_max(rho_rand)
+        v_est = CHSH_qnn(rho_rand)
+        print(f"CHSH max: {v_opt:.7f}")
+        print(f"Estimate: {v_est:.7f}")
+
+        if np.abs(v_opt - v_est) > 1e-5:
+            print(f"Error!")
+            print(rho_rand)
+            break
+
+
+def generate_positive(n: int, m: int) -> np.matrix:
+    P = np.zeros(shape=(n, n), dtype=np.complex64)
+    P = np.asmatrix(P)
+    for i in range(m):
+        v_re = np.random.random(size=(n, 1))
+        v_im = np.random.random(size=(n, 1))
+        v = np.asmatrix(v_re + 1j * v_im)
+        c = np.random.random()
+        P += c * v @ v.H
+    return P
+
+def generate_real_positive(n: int, m: int) -> np.matrix:
+    P = np.zeros(shape=(n, n), dtype=np.float64)
+    P = np.asmatrix(P)
+    for i in range(m):
+        v_re = np.random.random(size=(n, 1))
+        v = np.asmatrix(v_re)
+        c = np.random.random()
+        P += c * v @ v.T
+    return P
+
+def check_positive(P: np.matrix) -> bool:
+    # P is Hermitian
+    if not np.allclose(P, P.H):
+        return False
+    
+    es = np.linalg.eigvals(P)
+    for e in es:
+        # e should be real and e should be positive
+        if np.abs(e.imag) > 1e-5 or e < -1e-5 :
+            return False
+    return True
+
+
+def GHZ(sign: int) -> np.matrix:
+    ghz = np.matrix([1, 0, 0, 0, 0, 0, 0, 0]) + sign * np.matrix([0, 0, 0, 0, 0, 0, 0, 1])
+    ghz = ghz / np.sqrt(2)
+    ghz = ghz.H 
+    return ghz
+
+def generate_examples_Werner_states(p: float) -> Tuple[np.matrix, float]:
+    # randome choose |GHZ>+ or |GHZ>-
+    if np.random.random() < 0.5:
+        ghz = GHZ(1)
+    else:
+        ghz = GHZ(-1)
+    ans = p * ghz @ ghz.H
+    ans += (1 - p) * np.eye(8) / 8
+    return ans, 4 * np.sqrt(2) * p
+
+def run_Werner_states():
+    for p in np.linspace(0, 1, 20, dtype=np.float32):
+        rho, s = generate_examples_Werner_states(p)
+        print(f"p = {p: .7f}")
+        opt = s
+        est = Svetlichny_qnn(rho)
+        print(f"OPT = {opt: .7f}")
+        print(f"EST = {est: .7f}")
+        if np.abs(opt - est) > 1e-5:
+            print(f"Error! p = {p:.7f}")
+            break
+
+
+def generate_examples_GHZ_symmetric_states(p: float, q: float) -> Tuple[np.matrix, float]:
+    eps = 1e-5
+    if q < -1 / (4 * np.sqrt(3)) - eps or q > np.sqrt(4) / 4 + eps:
+        raise ValueError("q not in the range.")
+    if np.abs(p) > np.sqrt(3) / 2 * q + 1 / 8 + eps:
+        raise ValueError("p not in the range.")
+    ghz_p = GHZ(1)
+    ghz_m = GHZ(-1)
+    rho =  (2 * q / np.sqrt(3) + p) * ghz_p @ ghz_p.H
+    rho += (2 * q / np.sqrt(3) - p) * ghz_m @ ghz_m.H
+    rho += (1 - 4 * q / np.sqrt(3)) * np.eye(8) / 8
+    return rho, 8 * np.sqrt(2) * np.abs(p)
+
+def run_GHZ_symmetric_states():
+    # -1 / (4 * sqrt(3)) <= q <= sqrt(3) / 4
+    # |p| <= sqrt(3) * q / 2 + 1 / 8
+    q_min = -1 / (4 * np.sqrt(3))
+    q_max = np.sqrt(3) / 4
+
+    for q in np.linspace(q_min, q_max, 20, dtype=np.float32):
+        p_max = np.sqrt(3) * q / 2 + 1 / 8
+        for p in np.linspace(-p_max, p_max, 20, dtype=np.float32):
+            rho, s = generate_examples_GHZ_symmetric_states(p, q)
+            print(f"(p, q) = ({p: .7f}, {q: .7f})")
+            opt = s
+            est = Svetlichny_qnn(rho)
+            print(f"OPT = {opt: .7f}")
+            print(f"EST = {est: .7f}")
+            if np.abs(opt - est) > 1e-5:
+                print(f"Error! (p, q) = ({p:.7f}, {q:.7f})")
+                break
+
+
+def from_correlation_matrices3(T1: np.matrix, T2: np.matrix, T3: np.matrix) -> Optional[np.matrix]:
+    """Construct density operator from correlation matrices.
+
+    Args:
+        T1 (np.matrix): t[i,j,1]
+        T2 (np.matrix): t[i,j,2]
+        T3 (np.matrix): t[i,j,3]
+
+    Raises:
+        ValueError: T1, T2 and T3 must be 3x3 matrices.
+        TypeError: T1, T2 and T3 must be real matrices.
+
+    Returns:
+        np.matrix: Density operator.
+
+        .. math::
+            \rho = \frac{I}{8} + \frac{1}{8} \sum_{i,j,k=1}^3 t_{i,j,k} \sigma_i \otimes \sigma_j \otimes \sigma_k
+    """
+    
+    if not check_dim(T1, 3, 3) or not check_dim(T2, 3, 3) or not check_dim(T3, 3, 3):
+        raise ValueError("T1, T2 and T3 should be 3x3 matrices.")
+    if not np.allclose(T1.imag, np.zeros((3,3))) or not np.allclose(T2.imag, np.zeros((3,3))) or not np.allclose(T3.imag, np.zeros((3,3))):
+        raise TypeError("T1, T2 and T3 should be real matrices.")
+    
+    X = np.matrix([[0, 1],
+                   [1, 0]])
+    Y = np.matrix([[0, -1j],
+                   [1j, 0]])
+    Z = np.matrix([[1, 0],
+                   [0, -1]])
+    sigma = [X, Y, Z]
+    T = [T1, T2, T3]
+    rho = np.eye(8, 8, dtype=np.complex64)
+    rho = np.asmatrix(rho)
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                rho += T[k][i, j] * np.kron(np.kron(sigma[i], sigma[j]), sigma[k])
+    
+    rho /= rho.trace()
+
+    if not check_positive(rho):
+        return None
+    else:
+        return rho
+
+def from_correlation_matrices3_unchecked(T1: np.matrix, T2: np.matrix, T3: np.matrix) -> np.matrix:
+    if not check_dim(T1, 3, 3) or not check_dim(T2, 3, 3) or not check_dim(T3, 3, 3):
+        raise ValueError("T1, T2 and T3 should be 3x3 matrices.")
+    if not np.allclose(T1.imag, np.zeros((3,3))) or not np.allclose(T2.imag, np.zeros((3,3))) or not np.allclose(T3.imag, np.zeros((3,3))):
+        raise TypeError("T1, T2 and T3 should be real matrices.")
+    
+    X = np.matrix([[0, 1],
+                   [1, 0]])
+    Y = np.matrix([[0, -1j],
+                   [1j, 0]])
+    Z = np.matrix([[1, 0],
+                   [0, -1]])
+    sigma = [X, Y, Z]
+    T = [T1, T2, T3]
+    rho = np.eye(8, 8, dtype=np.complex64)
+    rho = np.asmatrix(rho)
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                rho += T[k][i, j] * np.kron(np.kron(sigma[i], sigma[j]), sigma[k])
+    
+    rho /= rho.trace()
+    return rho
+
+def from_correlation_matrices3_safe(T1: np.matrix, T2: np.matrix, T3: np.matrix) -> np.matrix:
+    if not check_dim(T1, 3, 3) or not check_dim(T2, 3, 3) or not check_dim(T3, 3, 3):
+        raise ValueError("T1, T2 and T3 should be 3x3 matrices.")
+    if not np.allclose(T1.imag, np.zeros((3,3))) or not np.allclose(T2.imag, np.zeros((3,3))) or not np.allclose(T3.imag, np.zeros((3,3))):
+        raise TypeError("T1, T2 and T3 should be real matrices.")
+    
+    X = np.matrix([[0, 1],
+                   [1, 0]])
+    Y = np.matrix([[0, -1j],
+                   [1j, 0]])
+    Z = np.matrix([[1, 0],
+                   [0, -1]])
+    sigma = [X, Y, Z]
+    T = [T1, T2, T3]
+    
+    rho = np.zeros(shape=(8, 8), dtype=np.complex64)
+    rho = np.asmatrix(rho)
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                rho += T[k][i, j] * np.kron(np.kron(sigma[i], sigma[j]), sigma[k])
+    
+    eigens = np.linalg.eigvals(rho)
+    eigen_min = eigens.real.min()
+    if eigen_min < 0:
+        rho -= eigen_min * np.eye(8, dtype=np.complex64)
+    
+    rho /= rho.trace()
+    return rho
+
+
+def to_correlation_matrices3(rho: np.matrix) -> Tuple[np.matrix, np.matrix, np.matrix]:
+    if not check_positive(rho):
+        raise ValueError("rho must be positive.")
+    if not np.allclose(rho.trace(), 1):
+        raise ValueError("The trace of rho must be 1.")
+    
+    X = np.matrix([[0, 1],
+                   [1, 0]])
+    Y = np.matrix([[0, -1j],
+                   [1j, 0]])
+    Z = np.matrix([[1, 0],
+                   [0, -1]])
+    sigma = [X, Y, Z]
+    T = np.zeros((3,3,3), dtype=np.float64)
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                T[i,j,k] = (rho @ np.kron(np.kron(sigma[i], sigma[j]), sigma[k])).trace().real
+    return np.asmatrix(T[:, :, 0]), np.asmatrix(T[:, :, 1]), np.asmatrix(T[:, :, 2])
+
+def generate_examples_onlyOne_correlation_matrix(m: int = 3) -> Tuple[np.matrix, float]:
+    T = generate_real_positive(3, m)
+    eigens = np.linalg.eigvals(T.T @ T)
+    eigens = eigens.real
+    eig_max = eigens.max()
+
+    p = np.random.random()
+    if p < 1/3:
+        rho = from_correlation_matrices3(T, np.zeros((3,3)), np.zeros((3,3)))
+    elif p < 2 / 3:
+        rho = from_correlation_matrices3(np.zeros((3,3)), T, np.zeros((3,3)))
+    else:
+        rho = from_correlation_matrices3(np.zeros((3,3)), np.zeros((3,3)), T)
+    
+    if rho is None:
+        return generate_examples_onlyOne_correlation_matrix(m)
+    else:
+        return rho, 4 * np.sqrt(eig_max)
+
+def run_onlyOne_correlation_matrix():
+    m = 3
+    for i in range(100):
+        rho, s = generate_examples_onlyOne_correlation_matrix(m)
+        opt = s
+        est = Svetlichny_qnn(rho)
+        print(f"i = {i:5}")
+        print(f"OPT = {opt:.7f}")
+        print(f"EST = {est:.7f}")
+
+        if np.abs(opt - est) > 1e-5:
+            print(f"Error! rho is:")
+            print(rho)
+            break
+
+
+def generate_examples_sphere(theta: np.float32, phi: np.float32) -> np.matrix:
+    psi1 = np.matrix([0, 1, 0, 0, 0, 0, 0, 0]) # row vector
+    psi2 = np.matrix([0, 0, 1, 0, 0, 0, 0, 0])
+    psi3 = np.matrix([0, 0, 0, 0, 1, 0, 0, 0])
+    a = np.sin(theta) * np.cos(phi)
+    b = np.sin(theta) * np.sin(phi)
+    c = np.cos(theta)
+    psi = a * psi1 + b * psi2 + c * psi3
+    rho = psi.H @ psi   # col * row
+    return rho  
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+def run_sphere():
+    n1, n2 = 50, 50
+
+    thetas = np.linspace(0, np.pi / 2, n1, dtype=np.float32)
+    phis = np.linspace(0, np.pi / 2, n2, dtype=np.float32)
+    estimates = []
+    for theta in thetas:
+        row = []
+        for phi in phis:
+            rho = generate_examples_sphere(theta, phi)
+            est = Svetlichny_qnn(rho)
+            row.append(est)
+            # if np.abs(est - 4.0) > 1e-3:
+            #     print(f"({theta:.7f}, {phi:.7f}): {est:.7f}")
+        estimates.append(row)
+
+    # store data
+    data = dict()
+    data["theta"] = thetas
+    data["phi"] = phis
+    data["estimate"] = estimates
+
+    with open(f"estimate_{n1}x{n2}_pi_div_2.json", "w") as f:
+        json.dump(data, f, cls=MyEncoder)
+
+    pd = pandas.DataFrame(estimates, columns=None)
+    pd.to_csv(f"estimate_{n1}x{n2}_pi_div_2.csv", header=None, index=0)
+
+
+if __name__ == "__main__":
+    print("hello world")
